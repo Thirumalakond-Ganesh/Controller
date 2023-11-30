3df2824f07c0f77e45dc3b07de40703a2c993200
Thirumalakonda-Ganesh <ganitthirumala50@gmail.com>
2023-11-30 22:29:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+
+import React from "react";
+import { useForm, Controller } from "react-hook-form";
+import "./App.css"; // Import the CSS file
 
 function App() {
+  const { control, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      
+      <form onSubmit={handleSubmit(onSubmit)} className="form-container">
+      <h2>Controller Example</h2>
+        <Controller
+          name="email"
+          control={control}
+          defaultValue=""
+          render={({ field, fieldState }) => (
+            <div className="input-container">
+              <label>Email:</label>
+              <input {...field} type="email" placeholder="Enter your email" />
+              {fieldState.error && <p className="error-message">{fieldState.error.message}</p>}
+            </div>
+          )}
+        />
+
+        <Controller
+          name="password"
+          control={control}
+          defaultValue=""
+          render={({ field, fieldState }) => (
+            <div className="input-container">
+              <label>Password:</label>
+              <input {...field} type="password" placeholder="Enter your password" />
+              {fieldState.error && <p className="error-message">{fieldState.error.message}</p>}
+            </div>
+          )}
+        />
+
+        <button type="submit" className="submit-button">
+          Log In
+        </button>
+      </form>
     </div>
   );
 }
